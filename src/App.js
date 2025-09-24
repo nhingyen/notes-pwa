8eb543c994188e1cd781994e41f90b0da29db66b
nhingyen <yennhing.work@gmail.com>
2025-09-24 10:05:25 +0700

    fix: to deploy by netlify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import NotesList from "./components/NotesList";
 import NoteEditor from "./components/NoteEditor";
 import SearchBar from "./components/SearchBar";
